0f1145fdf95d8a1912fde5a038cadc14f6747cfb
Danny Muskowsky <danny.muskowsky@inverso.de>
2022-09-16 13:45:01 +0200

    Basis für Projekt

--- MyLogging/src/skripte/MAIN.java
+++ MyLogging/src/skripte/MAIN.java
@@ -1,16 +1,9 @@
 package skripte;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class MAIN {
 
-    private static final Logger LOG = LogManager.getLogger(MAIN.class);
-
     public static void main(String[] args) {
-	LOG.info("info");
-	LOG.warn("warn");
-	LOG.error("error");
-	LOG.fatal("fatal");
+	System.out.println("Das ist ein Test");
     }
 
 }
