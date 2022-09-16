2b8388277a4de39ede1fbf193b9bd182ecd18e1b
Danny Muskowsky <danny.muskowsky@inverso.de>
2022-09-16 14:00:40 +0200

    rechner geandernt

--- MyLogging/src/skripte/MAIN.java
+++ MyLogging/src/skripte/MAIN.java
@@ -5,7 +5,7 @@ public class MAIN {
     public static void main(String[] args) {
 	System.out.println("Das ist ein Test");
 
-	System.out.println("1 + 1 ist 2.");
+	System.out.println("1 + 1 ist 2. ");
     }
 
 }
