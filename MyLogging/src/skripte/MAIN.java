de0e2f5d546a3e8ff00905f05712b22af79bc3cf
Danny Muskowsky <danny.muskowsky@inverso.de>
2022-09-16 13:55:27 +0200

    alles entfernt

--- MyLogging/src/skripte/MAIN.java
+++ MyLogging/src/skripte/MAIN.java
@@ -3,17 +3,7 @@ package skripte;
 public class MAIN {
 
     public static void main(String[] args) {
-<<<<<<< Upstream, based on Rechner
-<<<<<<< Upstream, based on Rechner
-	System.out.println("Das ist ein Test");
-
-	System.out.println("1 + 1 ist 2.");
-=======
-	System.out.println("Das ist ein Test. Dieses Programm verfügt über sehr viele Funktionen.");
->>>>>>> 17357a6 master: Beschreibung hinzugefügt
-=======
-	System.out.println("Dieses Programm verfügt über sehr viele Funktionen.");
->>>>>>> e83a92f master: Formatänderungen
+	//
     }
 
 }
