0dca52d056a05d79b15c6875e772f9e7da67b258
Danny Muskowsky <danny.muskowsky@inverso.de>
2022-09-16 13:54:10 +0200

    conflicts behoben

--- MyLogging/src/skripte/MAIN.java
+++ MyLogging/src/skripte/MAIN.java
@@ -3,9 +3,13 @@ package skripte;
 public class MAIN {
 
     public static void main(String[] args) {
+<<<<<<< Upstream, based on Rechner
 	System.out.println("Das ist ein Test");
 
 	System.out.println("1 + 1 ist 2.");
+=======
+	System.out.println("Das ist ein Test. Dieses Programm verfügt über sehr viele Funktionen.");
+>>>>>>> 17357a6 master: Beschreibung hinzugefügt
     }
 
 }
